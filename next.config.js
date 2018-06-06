c525628e7173e754b353bda6435e3844bd525dba
progfay <shun1856shun@gmail.com>
2018-06-06 18:51:26 +0900

    feat(manifest): add manifest.json

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
-module.exports = {
+const withManifest = require('next-manifest')
+
+module.exports = withManifest({
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
     }
-
     return config
+  },
+  manifest: {
+    "name"              : "eighteen",
+    "short_name"        : "18",
+    "description"       : "It's Rock-Scissors-Paper, but each player can play only 18 fingers in total can be used.",
+    "start_url"         : "/",
+    "display"           : "standalone",
+    "orientation"       : "any",
+    "background_color"  : "#FFF",
+    "theme_color"       : "#FFF",
+    "icons": {
+      src: './icon/icon.png',
+      cache: true
+    }
   }
-}
+})
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import Manifest from 'next-manifest/manifest'
 import Button from '../components/button'
 
 export default class Counter extends React.Component {
@@ -20,75 +21,69 @@ export default class Counter extends React.Component {
 
   render () {
     return (
-      <React.Fragment>
-        <Head>
-          <title> 18 </title>
-          <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' />
-        </Head>
-        <div id='container'>
-          <h1 id='count'> {this.state.count} </h1>
-          <div id='rock-scissors-paper'>
-            <Button onclick={this.reduce(0)} disabled={this.state.count < 0} name='✊' />
-            <Button onclick={this.reduce(2)} disabled={this.state.count < 2} name='✌' />
-            <Button onclick={this.reduce(5)} disabled={this.state.count < 5} name='✋' />
-          </div>
-          <div id='reset'>
-            <Button onclick={this.reset} disabled={false} name='Reset' />
-          </div>
-          <style jsx global>{`
-            * {
-              padding: 0;
-              margin: 0;
-            }
-            body {
-              width: 100vw;
-              height: 100vh;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              overflow: hidden;
-            }
-            button {
-              width: 100%;
-              margin: 0 10%;
-              height: 15vh;
-              font-size: 12vh;
-              background: none;
-              border: none;
-            }
-            button[disabled] {
-              opacity: 0.3;
-            }
-          `}</style>
-          <style jsx>{`
-            #container {
-              width: 80vw;
-              max-width: 960px;
-              height: 100vh;
-              display: flex;
-              justify-content: space-around;
-              align-items: center;
-              flex-direction: column;
-            }
-            #rock-scissors-paper {
-              width: 100%;
-              height: auto;
-              display: flex;
-              justify-content: space-around;
-              align-items: center;
-            }
-            #reset {
-              width: 50vw;
-              max-width: 480px;
-              display: flex;
-              justify-content: center;
-            }
-            #count {
-              font-size: 20vmin;
-            }
-          `}</style>
+      <div id='container'>
+        <h1 id='count'> {this.state.count} </h1>
+        <div id='rock-scissors-paper'>
+          <Button onclick={this.reduce(0)} disabled={this.state.count < 0} name='✊' />
+          <Button onclick={this.reduce(2)} disabled={this.state.count < 2} name='✌' />
+          <Button onclick={this.reduce(5)} disabled={this.state.count < 5} name='✋' />
         </div>
-      </React.Fragment>
+        <div id='reset'>
+          <Button onclick={this.reset} disabled={false} name='Reset' />
+        </div>
+        <style jsx global>{`
+          * {
+            padding: 0;
+            margin: 0;
+          }
+          body {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
+          }
+          button {
+            width: 100%;
+            margin: 0 10%;
+            height: 15vh;
+            font-size: 12vh;
+            background: none;
+            border: none;
+          }
+          button[disabled] {
+            opacity: 0.3;
+          }
+        `}</style>
+        <style jsx>{`
+          #container {
+            width: 80vw;
+            max-width: 960px;
+            height: 100vh;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            flex-direction: column;
+          }
+          #rock-scissors-paper {
+            width: 100%;
+            height: auto;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+          }
+          #reset {
+            width: 50vw;
+            max-width: 480px;
+            display: flex;
+            justify-content: center;
+          }
+          #count {
+            font-size: 20vmin;
+          }
+        `}</style>
+      </div>
     )
   }
 }
